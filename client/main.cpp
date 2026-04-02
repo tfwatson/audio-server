5d1bac4b645a91a85b12846f701c6dc7ca247acf
Tom Watson <tfwatson02@gmail.com>
2026-04-02 16:03:22 -0700

    Add signal handling for graceful exit

--- client/main.cpp
+++ client/main.cpp
@@ -1,16 +1,40 @@
+#include <atomic>
+#include <csignal>
 #include <iostream>
 #include <memory>
+#include <thread>
 
 #include "Listener.hpp"
 
+std::atomic<bool> running = true;
+
+void signalHandler(int signal)
+{
+    running = false;
+}
+
 int main()
 {
+    // Register interrupt signal to end the program gracefully
+    signal(SIGINT, signalHandler);
+
+    // Setup Listener instance
+    std::unique_ptr<Listener> listener;
     try
     {
-        auto listener = std::make_unique<Listener>(44100, 512, 2);
+        listener = std::make_unique<Listener>(44100, 512, 2);
     }
     catch (const std::runtime_error& error)
     {
         std::cerr << error.what() << '\n';
+        return 1;
     }
+
+    // Wait for interrupt signal to gracefully exit
+    while(running) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    // Exit successfully
+    return 0;
 }
